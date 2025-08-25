c775a63b0ab5f196493eea76d532c7962168e03e
ramsinghchy111 <ramsinghchaudhary800@gmail.com>
2025-08-25 10:31:16 +0000

    conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,6 @@
+let score = "ram"
+
+let valueInNumber = Number(score)
+console.log(typeof score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
---
+++ Basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 11112;
+let emailAccount = "ram@122"
+var passowrd = "12345"
+cityPlace = "kathmandu"
+let cityCode;
+
+emailAccount = "ram@222"
+passowrd = "11111"
+cityPlace = "bangalore"
+console.log([accountId, emailAccount, passowrd, cityPlace, cityCode])
