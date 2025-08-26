4771c72012c4760beb78d6fe07dc22446a1553fc
ramsinghchy111 <ramsinghchaudhary800@gmail.com>
2025-08-26 05:28:45 +0000

    math and nums

---
+++ Basics/num.js
@@ -0,0 +1,12 @@
+let number = 100
+console.log(number)
+const  balanace = new Number(1000)
+console.log(balanace.toFixed(2))
+const decimal = 100000000
+console.log(decimal.toLocaleString('en-In'))
+
+// +++++++++++++ math +++++++++++++++++++
+console.log(Math.abs(-4))
+console.log(Math.round(44.2))
+console.log(Math.floor(1.9))
+console.log(Math.ceil(99.1))
